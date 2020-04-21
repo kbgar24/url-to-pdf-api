5fb4f977ee5699f56326ea72edd65e46e2268932
Kendrick Gardner <kbgar24@gmail.com>
2020-04-21 06:46:08 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,16 +2,12 @@
 
 // Env vars should be casted to correct types
 const config = {
-  // PORT: Number(process.env.PORT) || 9000,
-  PORT: 9009,
-  // NODE_ENV: "process.env.NODE_ENV",
-  NODE_ENV: "production",
+  PORT: Number(process.env.PORT) || 9000,
+  NODE_ENV: "process.env.NODE_ENV",
   LOG_LEVEL: process.env.LOG_LEVEL,
-  // ALLOW_HTTP: process.env.ALLOW_HTTP === 'true',
-  ALLOW_HTTP: true,
+  ALLOW_HTTP: process.env.ALLOW_HTTP === 'true',
   DEBUG_MODE: process.env.DEBUG_MODE === 'true',
-  // CORS_ORIGIN: process.env.CORS_ORIGIN || '*',
-  CORS_ORIGIN:  '*',
+  CORS_ORIGIN: process.env.CORS_ORIGIN || '*',
   BROWSER_WS_ENDPOINT: process.env.BROWSER_WS_ENDPOINT,
   BROWSER_EXECUTABLE_PATH: process.env.BROWSER_EXECUTABLE_PATH,
   API_TOKENS: [],
